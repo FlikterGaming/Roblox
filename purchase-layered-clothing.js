bd5648a461c2cf2d3754568cf3645e03d5af0636
FlikterGaming <83476766+FlikterGaming@users.noreply.github.com>
2022-01-31 10:14:04 +0100

    Update purchase-layered-clothing.js

--- purchase-layered-clothing.js
+++ purchase-layered-clothing.js
@@ -1,4 +1,13 @@
-//This uses the public economy api Roblox created for users to use. You will not get banned for this because it is an intended feature Roblox added.
+//How to use:
+//Go on the Roblox website page at the layered clothing item you want to purchase.
+//Open the console window in your browser (in most browsers you press Ctrl + Shift + I on your keyboard and then click the "Console" button at the top of the newly oppened window.
+//Now you can copy and paste this code inside the console command line.
+//Then, copy the product id of the layered clothing item by inspecting the purchase button using inspect element and looking inside the "s of the "data-product-id" tag.
+//In the ProductId variable inside the code located in the console command line, remove the number and paste the product id number as the new value and run the code by pressing Enter on your keyboard.
+//It should work shortly, if it doesn't, Roblox fixed this way of purchasing layered clothing items.
+//More info is available in the code below.
+
+//This uses the public economy api Roblox created for users to use. You will not get banned for this because it is an intended feature Roblox added. This will not steal your Roblox account or any associated info.
 
 var ProductId = 1235118569 //Change this to the product id of the item you want to buy. This will remove the amount of robux this item costs from your account.
 
